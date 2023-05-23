7db947585f0ae4508aa242ef4de8394c525e0991
Sergey Koltunov <sergeusprecious@gmail.com>
2023-05-23 23:54:32 +0300

    feat/add alembic

--- src/db/database.py
+++ src/db/database.py
@@ -1,9 +1,11 @@
+import os
+
 from sqlalchemy import create_engine, MetaData
 # from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL") #postgres:// не поддерживается sqlalchemy
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
