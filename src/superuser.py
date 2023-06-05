f9d359ae038dbba2d5ac7988bf3f12780952a318
Sergey Koltunov <sergeusprecious@gmail.com>
2023-06-05 23:29:21 +0300

    feat:add superuser script, envs fix:paths

---
+++ src/superuser.py
@@ -0,0 +1,33 @@
+import os
+from contextlib import contextmanager, closing
+
+import psycopg2
+from psycopg2.extensions import connection as _connection
+from psycopg2.extras import DictCursor
+
+from core.password.password import PasswordHelper
+
+dsl = {'dbname': os.getenv('POSTGRES_DB'), 'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('DB_PASSWORD'), 'host': os.getenv('POSTGRES_HOST'), 
+    'port': os.getenv('POSTGRES_PORT')}
+
+
+password_hasher = PasswordHelper()
+superuser_password = password_hasher.hash(os.getenv('SUPER_USER_PW'))
+
+
+
+if __name__ == '__main__':
+    with closing(psycopg2.connect(**dsl)) as pg_conn:
+        curs = pg_conn.cursor()
+        try:
+            curs.execute(f"""
+                INSERT INTO users.user (username, email, hashed_password, is_active, is_superuser, first_name, last_name)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT (username) DO UPDATE SET
+                (username, email, hashed_password, is_active, is_superuser, first_name, last_name) 
+                = (EXCLUDED.username, EXCLUDED.email, EXCLUDED.hashed_password, EXCLUDED.is_active, EXCLUDED.is_superuser, EXCLUDED.first_name, EXCLUDED.last_name);
+            """, ("superuser", "superuser@movix.com", superuser_password, True, True, "super", "user"))
+        except Exception as e:
+            exit(code=1)
+        pg_conn.commit()
