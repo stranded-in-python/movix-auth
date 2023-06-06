6665805901079ccb538a48bfeb3c988cadd7b5b6
Sergey Koltunov <sergeusprecious@gmail.com>
2023-06-06 16:14:46 +0300

    add superuser_id, fix paths in compose

--- src/superuser.py
+++ src/superuser.py
@@ -8,26 +8,28 @@ from psycopg2.extras import DictCursor
 from core.password.password import PasswordHelper
 
 dsl = {'dbname': os.getenv('POSTGRES_DB'), 'user': os.getenv('POSTGRES_USER'),
-    'password': os.getenv('DB_PASSWORD'), 'host': os.getenv('POSTGRES_HOST'), 
+    'password': os.getenv('POSTGRES_PASSWORD'), 'host': os.getenv('POSTGRES_HOST'), 
     'port': os.getenv('POSTGRES_PORT')}
+
+# dsl = {'dbname': os.getenv('POSTGRES_DB'), 'user': os.getenv('POSTGRES_USER'),
+#      'password': os.getenv('POSTGRES_PASSWORD'), 'host': "localhost", 
+#      'port': 5434}
 
 
 password_hasher = PasswordHelper()
-superuser_password = password_hasher.hash(os.getenv('SUPER_USER_PW'))
 
+superuser_password = password_hasher.hash(os.getenv('SUPER_USER_PW'))
+superuser_id = os.getenv('SUPER_USER_ID')
 
 
 if __name__ == '__main__':
     with closing(psycopg2.connect(**dsl)) as pg_conn:
         curs = pg_conn.cursor()
-        try:
-            curs.execute(f"""
-                INSERT INTO users.user (username, email, hashed_password, is_active, is_superuser, first_name, last_name)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (username) DO UPDATE SET
-                (username, email, hashed_password, is_active, is_superuser, first_name, last_name) 
-                = (EXCLUDED.username, EXCLUDED.email, EXCLUDED.hashed_password, EXCLUDED.is_active, EXCLUDED.is_superuser, EXCLUDED.first_name, EXCLUDED.last_name);
-            """, ("superuser", "superuser@movix.com", superuser_password, True, True, "super", "user"))
-        except Exception as e:
-            exit(code=1)
+        curs.execute(f"""
+            INSERT INTO users.user (id, username, email, hashed_password, is_active, is_superuser, first_name, last_name)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (id) DO UPDATE SET
+            (id, username, email, hashed_password, is_active, is_superuser, first_name, last_name) 
+            = (EXCLUDED.id, EXCLUDED.username, EXCLUDED.email, EXCLUDED.hashed_password, EXCLUDED.is_active, EXCLUDED.is_superuser, EXCLUDED.first_name, EXCLUDED.last_name);
+        """, (superuser_id, "superuser", "superuser@movix.com", superuser_password, True, True, "super", "user"))
         pg_conn.commit()
