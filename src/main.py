dbc17373679d191b38db4a3b7a00345f14fba1d1
Victor_Gostyaikin <v.gostyaikin@itworks.group>
2023-06-09 04:35:30 +0300

    fix: alembic migration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(
+    uvicorn.run(  # pyright: ignore
         "app:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=['/app']
-    )  # pyright: ignore
+    )
