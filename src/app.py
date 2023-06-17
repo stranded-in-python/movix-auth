28ba7fe3042f9ddd644bd71a816bb2f8cdbded35
sbkubric <sbkubric@gmail.com>
2023-06-17 23:23:27 +0300

    wip: prefix now /auth/api/v1/

--- src/app.py
+++ src/app.py
@@ -4,11 +4,16 @@ from fastapi.responses import ORJSONResponse
 from api import container, schemas
 from core.config import settings
 from core.logger import logger
+from core.tracers import configure_tracer, instrumentor
 from managers.user import google_oauth_client
 
 logger()
 
-app = FastAPI(
+configure_tracer()
+
+app = FastAPI(title=settings.project_name, default_response_class=ORJSONResponse)
+
+subapp = FastAPI(
     title=settings.project_name,
     docs_url="/api/v1/openapi",
     openapi_url="/api/v1/openapi.json",
@@ -16,37 +21,27 @@ app = FastAPI(
 )
 
 
-@app.middleware("http")
-async def require_request_id(request: Request, call_next):
-    request_id = request.headers.get('X-Request-Id')
-    if not request_id:
-        logger.exception('RuntimeError: HEADER X-Request-Id is required')
-        raise RuntimeError('RuntimeError: HEADER X-Request-Id is required')
-    response = await call_next(request)
-    return response
-
-
-app.include_router(
+subapp.include_router(
     container.api_users.get_register_router(schemas.UserRead, schemas.UserCreate),
     tags=["register"],
 )
-app.include_router(
+subapp.include_router(
     container.api_users.get_auth_router(
         container.access_backend, refresh_backend=container.refresh_backend
     ),
     tags=["auth"],
 )
-app.include_router(
+subapp.include_router(
     container.api_users.get_users_me_router(
         schemas.UserRead, schemas.UserUpdate, schemas.EventRead
     ),
     tags=["users"],
 )
-app.include_router(
+subapp.include_router(
     container.api_users.get_users_router(schemas.UserRead, schemas.UserUpdate),
     tags=["users"],
 )
-app.include_router(
+subapp.include_router(
     container.api_roles.get_roles_router(
         schemas.RoleRead,
         schemas.RoleCreate,
@@ -56,7 +51,7 @@ app.include_router(
     ),
     tags=["roles"],
 )
-app.include_router(
+subapp.include_router(
     container.api_access_rights.get_access_rights_router(
         schemas.AccessRightRead,
         schemas.AccessRightCreate,
@@ -66,7 +61,7 @@ app.include_router(
     ),
     tags=["access rights"],
 )
-app.include_router(
+subapp.include_router(
     container.api_users.get_oauth_router(
         google_oauth_client, container.access_backend, settings.state_secret
     ),
@@ -75,11 +70,16 @@ app.include_router(
 )
 
 
-@app.on_event("startup")
-async def on_startup():
-    ...
+@subapp.middleware("http")
+async def require_request_id(request: Request, call_next):
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        logger().exception('RuntimeError: HEADER X-Request-Id is required')
+        raise RuntimeError('RuntimeError: HEADER X-Request-Id is required')
+    response = await call_next(request)
+    return response
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    ...
+app.mount("/auth", subapp)
+
+instrumentor().instrument_app(app)  # type: ignore
